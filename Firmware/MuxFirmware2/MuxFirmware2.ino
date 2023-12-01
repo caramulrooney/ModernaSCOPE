93502267de62b44a71b3e80a4e821b768845f0ac
Ian Eykamp <ian@eykamp.com>
2023-12-01 09:40:11 -0500

    add ADS1110 modified ADS1110 library

--- Firmware/MuxFirmware2/MuxFirmware2.ino
+++ Firmware/MuxFirmware2/MuxFirmware2.ino
@@ -1,10 +1,23 @@
 #include <Wire.h>
 #include <math.h>
-#include "ADS1110.h" // Implement this library!! https://github.com/nadavmatalon/ADS1110/tree/master
+#include <ADS1110.h> // Implement this library!! https://github.com/nadavmatalon/ADS1110/tree/master
 
 // Define the ADS1110 I2C address
 // ADS1110_I2C_ADDRESS(0b1001001);
 #define ADS1110_I2C_ADDRESS 0b1001001
+ADS1110 ads1110(ADS1110_I2C_ADDRESS);
+#define ADC_MIN 100
+#define ADC_MAX 800
+
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
 
 #define MUX_SELECT_1 D0
 #define MUX_SELECT_2 D3
@@ -17,6 +30,59 @@
 #define nSelectPins 7
 int selectPins[] = {D0, D3, D4, D5, D6, D7, D8};
 
+
+
+// typedef enum:byte {
+//   GAIN_MASK = 0x03,      // 3 - B00000011
+//   GAIN_1    = 0x00,      // 0 - B00000000 (Default)
+//   GAIN_2    = 0x01,      // 1 - B00000001
+//   GAIN_4    = 0x02,      // 2 - B00000010
+//   GAIN_8    = 0x03       // 3 - B00000011
+// } gain_t;
+
+// typedef enum:byte {
+//   SPS_MASK = 0x0C,       // 12 - B00001100
+//   SPS_15   = 0x0C,       // 12 - B00001100 (Default)
+//   SPS_30   = 0x08,       //  8 - B00001000
+//   SPS_60   = 0x04,       //  4 - B00000100
+//   SPS_240  = 0x00        //  0 - B00000000
+// } sample_rate_t;
+
+// typedef enum:byte {
+//   CONT          = 0x00,  // B00000000 (Defualt)
+//   SINGLE        = 0x10   // B00010000
+// } con_mode_t;
+
+// typedef enum:byte {
+//   MIN_CODE_240 = 0x01,   //  1 - Minimal Data Value for 240_SPS / -2048  (12-BIT)
+//   MIN_CODE_60  = 0x04,   //  4 - Minimal Data Value for 60_SPS  / -2048  (14-BIT)
+//   MIN_CODE_30  = 0x08,   //  8 - Minimal Data Value for 30_SPS  / -2048  (15-BIT)
+//   MIN_CODE_15  = 0x10    // 16 - Minimal Data Value for 15_SPS  / -2048  (16-BIT) (Default)
+// } min_code_t;
+
+// typedef enum:byte {
+//   RES_12,                // 12-BIT Resolution
+//   RES_14,                // 14-BIT Resolution
+//   RES_15,                // 15-BIT Resolution
+//   RES_16                 // 16-BIT Resolution (Default)
+// } res_t;
+
+// typedef enum:int {
+//   INT_REF =    0,        // Inernal Reference:  Pin Vin- is connected to GND (Default)
+//   EXT_REF = 2048         // External Reference: Pin Vin- is connected to 2.048V source
+// } vref_t;
+
+// const byte DEFAULT_CONFIG = 12;      // B00001100 (16-BIT, 15 SPS, GAIN x1, CONTINUOUS)
+// byte _config = DEFAULT_CONFIG;
+// const byte COM_SUCCESS = 0;      // I2C Communication Success (No Error)
+// byte _comBuffer = COM_SUCCESS;
+
+// void setRes(res_t newRes);
+// void setGain(gain_t newGain);
+// void initCall(byte data);
+// void endCall();
+// void setConfig(byte newConfig);
+
 void setup() {
   // Initialize the Serial communication for debugging
   Serial.begin(9600);
@@ -28,7 +94,8 @@ void setup() {
   }
 
   // Set gain
-  setGain(GAIN_1);
+  ads1110.setGain(GAIN_1);
+  // setGain(GAIN_1);
 
   Serial.println("ADS1110 Initialized!");
 
@@ -42,7 +109,8 @@ void loop() {
     delay(10);
 
     // Read ADC and perform necessary operations
-    int adcValue = readADC();
+    int adcValue = ads1110.getData();
+    // int adcValue = readADC();
 
     // Print the result
     Serial.print("Electrode ");
@@ -108,3 +176,52 @@ float convertToPH(int adcValue) {
     return pH;
     // Replace ADC_MIN and ADC_MAX with actual calibration values
 }
+
+
+// /*==============================================================================================================*
+//     SET RESOLUTION
+//  *==============================================================================================================*/
+
+// void setRes(res_t newRes) {                             // PARAMS: 12_BIT / 14_BIT / 15_BIT / 16_BIT
+//     switch (newRes) {
+//         case (RES_12): setConfig((_config & ~SPS_MASK) | (SPS_240 & SPS_MASK)); break;
+//         case (RES_14): setConfig((_config & ~SPS_MASK) | (SPS_60  & SPS_MASK)); break;
+//         case (RES_15): setConfig((_config & ~SPS_MASK) | (SPS_30  & SPS_MASK)); break;
+//         case (RES_16): setConfig((_config & ~SPS_MASK) | (SPS_15  & SPS_MASK)); break;
+//     }
+// }
+
+// /*==============================================================================================================*
+//     SET GAIN
+//  *==============================================================================================================*/
+
+// void setGain(gain_t newGain) {                          // PARAMS: GAIN_1 / GAIN_2 / GAIN_4 / GAIN_8
+//     setConfig((_config & ~GAIN_MASK) | (newGain & GAIN_MASK));
+// }
+
+// /*==============================================================================================================*
+//     INITIATE I2C COMMUNICATION
+//  *==============================================================================================================*/
+
+// void initCall(byte data) {
+//     Wire.beginTransmission(ADS1110_I2C_ADDRESS);
+//     Wire.write(data);
+// }
+
+// /*==============================================================================================================*
+//     END I2C COMMUNICATION
+//  *==============================================================================================================*/
+
+// void endCall() {
+//     _comBuffer = Wire.endTransmission();
+// }
+
+// /*==============================================================================================================*
+//     SET CONFIGURATION REGISTER
+//  *==============================================================================================================*/
+
+// void setConfig(byte newConfig) {
+//     initCall(newConfig);
+//     endCall();
+//     if (_comBuffer == COM_SUCCESS) _config = newConfig;
+// }
