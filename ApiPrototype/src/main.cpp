091ef9fa21622f092bd4e4d52d73ae5d3bed258c
Ian Eykamp <ian@eykamp.com>
2024-02-02 13:30:13 -0500

    update api prototype

--- ApiPrototype/src/main.cpp
+++ ApiPrototype/src/main.cpp
@@ -13,24 +13,53 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <LittleFS.h>
 
 const char *ssid = "ESP8266-Access-Point";
 const char *password = "123456789";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+void writeFile(const char *, const char *);
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0; // will store last time DHT was updated
-
-// Updates DHT readings every 10 seconds
-const long interval = 10000;
+void checkFiles()
+{
+  if (LittleFS.exists("/index.html"))
+  {
+    Serial.println("/index.html exists.");
+  }
+  else
+  {
+    Serial.println("/index.html does not exist.");
+  }
+  if (LittleFS.exists("/style.css"))
+  {
+    Serial.println("/style.css exists.");
+  }
+  else
+  {
+    Serial.println("/style.css does not exist.");
+  }
+  if (LittleFS.exists("/result.txt"))
+  {
+    Serial.println("/result.txt exists.");
+  }
+  else
+  {
+    Serial.println("/result.txt does not exist.");
+  }
+}
 
 void setup()
 {
   // Serial port for debugging purposes
   Serial.begin(115200);
+  Serial.println();
+  Serial.println("Hello World!");
+  Serial.println();
+  writeFile("/result.txt", "a, b, c\n100, 200, 300\n300, 400, 100\n150, 300, 200");
+  checkFiles();
 
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
@@ -45,11 +74,16 @@ void setup()
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/html", "<b>Hello World!</b><br><br>Ask me about /temperature and /humidity."); });
+            { request->send(LittleFS, "text/html", "/index.html"); });
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "text/css", "/style.css"); });
   server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send_P(200, "text/plain", "This is your ~~ Temperature ~~"); });
   server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send_P(200, "text/plain", "This is your ~~~~ Humidity ~~~~"); });
+  server.on("/csv", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/plain", "/result.txt"); });
 
   // Start server
   server.begin();
@@ -57,4 +91,25 @@ void setup()
 
 void loop()
 {
+}
+
+void writeFile(const char *path, const char *message)
+{
+  Serial.printf("Writing file: %s\r\n", path);
+
+  File file = LittleFS.open(path, "w+");
+  if (!file)
+  {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.print(message))
+  {
+    Serial.println("- file written");
+  }
+  else
+  {
+    Serial.println("- write failed");
+  }
+  file.close();
 }
