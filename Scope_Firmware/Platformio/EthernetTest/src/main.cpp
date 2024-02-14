868f273ebc3ce152d6bf7cc3b8a5676c081e6c6f
Ian Eykamp <ian@eykamp.com>
2024-02-14 16:32:10 -0500

    create ethernet project

---
+++ Scope_Firmware/Platformio/EthernetTest/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+// put function declarations here:
+/*
+  ModbusTCP for W5x00 Ethernet library
+  Basic Server code example
+
+  (c)2020 Alexander Emelianov (a.m.emelianov@gmail.com)
+  https://github.com/emelianov/modbus-esp8266
+
+  This code is licensed under the BSD New License. See LICENSE.txt for more info.
+*/
+
+#include <SPI.h>
+#include <Ethernet.h> // Ethernet library v2 is required
+#include <ModbusEthernet.h>
+
+// Enter a MAC address and IP address for your controller below.
+byte mac[] = {
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 30, 177); // The IP address will be dependent on your local network:
+ModbusEthernet mb;               // Declare ModbusTCP instance
+
+void setup()
+{
+  Serial.begin(115200); // Open serial communications and wait for port to open
+
+  Ethernet.init(5);        // SS pin
+  Ethernet.begin(mac, ip); // start the Ethernet connection
+  delay(1000);             // give the Ethernet shield a second to initialize
+  mb.server();             // Act as Modbus TCP server
+  mb.addReg(HREG(100));    // Add Holding register #100
+}
+
+void loop()
+{
+  mb.task(); // Server Modbus TCP queries
+  delay(50);
+}
