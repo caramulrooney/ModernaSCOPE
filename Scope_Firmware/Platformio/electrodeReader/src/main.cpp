72404de5bc0e5b8e587f2a3b25fbfc9d204d7b87
Cara Mulrooney <cmulrooney@olin.edu>
2024-02-28 08:36:31 -0500

    Add calculateVoltage function

--- Scope_Firmware/Platformio/electrodeReader/src/main.cpp
+++ Scope_Firmware/Platformio/electrodeReader/src/main.cpp
@@ -6,8 +6,43 @@
 const int NUM_ELECTRODES = 96;
 float voltages[NUM_ELECTRODES];
 
+// Define the I2C address for the ADS1110
+#define ads1110 0x49
+
+// Define ADC select pins
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+
+#define nSelectPins 7
+int selectPins[] = {D0, D3, D4, D5, D6, D7, D8};
+
+bool fHasLooped = false;
+int electrode = 1; //Looking at one electrode at a time
+
+// Declare variables to store voltage and ads1110 raw data
+float voltage, data;
+
+// Declare variables for high byte, low byte, and configuration register
+byte highbyte, lowbyte, configRegister;
+
 void setup() {
   Serial.begin(115200);
+  // Initialize the wire library for I2C communication
+  Wire.begin();
+  pinMode(D0, OUTPUT);
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT); 
 }
 
 void loop() {
@@ -17,7 +52,7 @@ void loop() {
       // read voltages from electrodes
       for (int i = 0; i <= NUM_ELECTRODES; i++) {
         voltages[i] = 1;
-        //analogRead(i) * (2.048 / 1023.0) // CHANGE
+        //calculateVoltage(i);
       }
       // format data in JSON
       JsonDocument doc; // create static json document with capacity of 256 bytes
@@ -34,3 +69,46 @@ void loop() {
   }
   delay(10);
 }
+
+/*
+void calculateVoltage(int electrode){
+  selectElectrode(electrode);
+  
+  // Request 3 bytes of data from the ADS1110 via I2C
+  Wire.requestFrom(ads1110, 3);
+  // Wait until all the requested data is available
+  while(Wire.available()) // ensure all the data comes in
+  {
+    // Read the high byte, low byte, and configuration register
+    highbyte = Wire.read(); // high byte * B11111111
+    lowbyte = Wire.read(); // low byte
+    configRegister = Wire.read();
+  }
+  
+  // Combine the high and low bytes to get the raw data
+  data = highbyte * 256;
+  data = data + lowbyte;
+
+  // Convert the raw data to voltage (assuming 2.048V reference and 15-bit resolution)
+  voltage = data * 2.048 ;
+  voltage = voltage / 32768.0;
+  voltages[electrode] = voltage
+
+  // Delay for 1 second
+  delay(1000);
+        
+}
+
+void selectElectrode(int electrode){
+  Serial.print("Selecting electrode "); 
+  Serial.print(electrode); 
+  Serial.print(". Select outputs are: ");
+  
+  for (int pin = 0; pin < nSelectPins; pin++) {
+    bool pinState = (electrode >> pin) % 2; // get the nth bit of the 7-bit binary number
+    digitalWrite(selectPins[pin], pinState);
+    Serial.print(int(pinState)); 
+    Serial.print(" ");
+  }
+}
+*/
