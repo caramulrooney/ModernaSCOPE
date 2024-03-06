c62f2f5528c0ed324db593750de00a1d3b973145
Ian Eykamp <ian@eykamp.com>
2024-03-06 11:00:28 -0500

    dynamic plotting of a single electrode voltage

--- Scope_Firmware/Platformio/electrodeReader/src/main.cpp
+++ Scope_Firmware/Platformio/electrodeReader/src/main.cpp
@@ -3,8 +3,11 @@
 #include <SPI.h>
 #include <Wire.h>
 
-const int NUM_ELECTRODES = 96;
-float voltages[NUM_ELECTRODES];
+#define NUM_ELECTRODES 96
+// #define DEBUG
+
+void selectElectrode(int electrode_id);
+float calculateVoltage(int electrode);
 
 // Define the I2C address for the ADS1110
 #define ads1110 0x49
@@ -22,93 +25,88 @@ float voltages[NUM_ELECTRODES];
 
 #define nSelectPins 7
 int selectPins[] = {D0, D3, D4, D5, D6, D7, D8};
-
-bool fHasLooped = false;
-int electrode = 1; //Looking at one electrode at a time
-
-// Declare variables to store voltage and ads1110 raw data
-float voltage, data;
+float voltages[NUM_ELECTRODES];
 
 // Declare variables for high byte, low byte, and configuration register
 byte highbyte, lowbyte, configRegister;
 
 void setup() {
-  Serial.begin(115200);
-  // Initialize the wire library for I2C communication
-  Wire.begin();
-  pinMode(D0, OUTPUT);
-  pinMode(D3, OUTPUT);
-  pinMode(D4, OUTPUT);
-  pinMode(D5, OUTPUT);
-  pinMode(D6, OUTPUT);
-  pinMode(D7, OUTPUT);
-  pinMode(D8, OUTPUT); 
+    Serial.begin(115200);
+    // Initialize the wire library for I2C communication
+    Wire.begin();
+    pinMode(D0, OUTPUT);
+    pinMode(D3, OUTPUT);
+    pinMode(D4, OUTPUT);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D7, OUTPUT);
+    pinMode(D8, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    char receivedChar = Serial.read();
-    if (receivedChar == 'e') {
-      // read voltages from electrodes
-      for (int i = 0; i <= NUM_ELECTRODES; i++) {
-        voltages[i] = 1;
-        //calculateVoltage(i);
-      }
-      // format data in JSON
-      JsonDocument doc; // create static json document with capacity of 256 bytes
-      JsonArray values = doc.to<JsonArray>(); // create json array `values` and initialize it with contents of json doc
-      for (int i = 0; i < NUM_ELECTRODES; i++) { // iterate over each element in  `voltages` array and add them to json array
-        values.add(voltages[i]);
-      }
+    if (Serial.available() > 0) {
+        char receivedChar = Serial.read();
+        if (receivedChar == 'e') {
+            // read voltages from electrodes
+            for (int i = 0; i < NUM_ELECTRODES; i++) {
+                voltages[i] = calculateVoltage(i);
+            }
 
-      // send json back to python script
-      serializeJson(values, Serial); // serialize json array into json string and write to serial port
-      Serial.println();
+            // format data in JSON
+            JsonDocument doc;                           // create static json document with capacity of 256 bytes
+            JsonArray values = doc.to<JsonArray>();     // create json array `values` and initialize it with contents of json doc
+            for (int i = 0; i < NUM_ELECTRODES; i++) {  // iterate over each element in  `voltages` array and add them to json array
+                values.add(voltages[i]);
+            }
 
+            // send json back to python script
+            serializeJson(values, Serial);  // serialize json array into json string and write to serial port
+            Serial.println();
+        }
     }
-  }
-  delay(10);
+    delay(10);
 }
 
-/*
-void calculateVoltage(int electrode){
-  selectElectrode(electrode);
-  
-  // Request 3 bytes of data from the ADS1110 via I2C
-  Wire.requestFrom(ads1110, 3);
-  // Wait until all the requested data is available
-  while(Wire.available()) // ensure all the data comes in
-  {
-    // Read the high byte, low byte, and configuration register
-    highbyte = Wire.read(); // high byte * B11111111
-    lowbyte = Wire.read(); // low byte
-    configRegister = Wire.read();
-  }
-  
-  // Combine the high and low bytes to get the raw data
-  data = highbyte * 256;
-  data = data + lowbyte;
+float calculateVoltage(int electrode) {
+    selectElectrode(electrode);
 
-  // Convert the raw data to voltage (assuming 2.048V reference and 15-bit resolution)
-  voltage = data * 2.048 ;
-  voltage = voltage / 32768.0;
-  voltages[electrode] = voltage
+    // Request 3 bytes of data from the ADS1110 via I2C
+    Wire.requestFrom(ads1110, 3);
+    // Wait until all the requested data is available
+    while (Wire.available())  // ensure all the data comes in
+    {
+        // Read the high byte, low byte, and configuration register
+        highbyte = Wire.read();  // high byte * B11111111
+        lowbyte = Wire.read();   // low byte
+        configRegister = Wire.read();
+    }
 
-  // Delay for 1 second
-  delay(1000);
-        
+    // Combine the high and low bytes to get the raw data
+    float data;
+    data = highbyte * 256;
+    data = data + lowbyte;
+
+    // Convert the raw data to voltage (assuming 2.048V reference and 15-bit resolution)
+    float voltage;
+    voltage = data * 2.04;
+    voltage = voltage / 32768.0;
+
+    return voltage;
 }
 
-void selectElectrode(int electrode){
-  Serial.print("Selecting electrode "); 
-  Serial.print(electrode); 
-  Serial.print(". Select outputs are: ");
-  
-  for (int pin = 0; pin < nSelectPins; pin++) {
-    bool pinState = (electrode >> pin) % 2; // get the nth bit of the 7-bit binary number
-    digitalWrite(selectPins[pin], pinState);
-    Serial.print(int(pinState)); 
-    Serial.print(" ");
-  }
+void selectElectrode(int electrode) {
+#ifdef DEBUG
+    Serial.print("Selecting electrode ");
+    Serial.print(electrode);
+    Serial.print(". Select outputs are: ");
+#endif
+
+    for (int pin = 0; pin < nSelectPins; pin++) {
+        bool pinState = (electrode >> pin) % 2;  // get the nth bit of the 7-bit binary number
+        digitalWrite(selectPins[pin], pinState);
+#ifdef DEBUG
+        Serial.print(int(pinState));
+        Serial.print(" ");
+#endif
+    }
 }
-*/
--- Scope_Firmware/Python/pythonApi/electrodeReader.py
+++ Scope_Firmware/Python/pythonApi/electrodeReader.py
@@ -1,14 +1,41 @@
 # Import libraries
 import serial
 import json
+import matplotlib.pyplot as plt
+import time
+
+from drawnow import drawnow
+
+which_electrode = 0
 
 def request_electrode_data():
-    with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser: # TO DO: change serial port
+    # with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser: # TO DO: change serial port
+    with serial.Serial('COM4', 115200, timeout=1) as ser: # TO DO: change serial port
         ser.write(b'e') # send request for electrode data
         response = ser.readline().decode().strip() # read response
         electrode_data = json.loads(response) # parse json data
         print("Electrode voltages: ", electrode_data)
+        return electrode_data
 
 if __name__ == "__main__":
-    request_electrode_data()
+    # plt.ion()
+    voltage_at_electrode = []
+    ts = []
+    now = 0
+    fig, ax1 = plt.subplots(1, 1, figsize = (6, 6))
+
+    while True:
+        data = request_electrode_data()
+        voltage_at_electrode.append(data[which_electrode])
+        ts.append(now)
+        now = now + 1
+
+        ax1.scatter(ts, voltage_at_electrode, color = "red")
+        ax1.plot(ts, voltage_at_electrode, color = "red")
+        ax1.set_xlabel("Measurement index (roughly one per second)")
+        ax1.set_ylabel(f"Voltage at electrode {which_electrode}")
+        fig.canvas.draw_idle()
+        plt.pause(1)
+
+
 
